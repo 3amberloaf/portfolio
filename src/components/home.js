8eec5374f876abd45d2eeccad0686847e6b3f040
Amber Sautner <ambersautner3@gmail.com>
2024-09-24 13:13:28 -0400

    added proiximo font

--- src/components/home.js
+++ src/components/home.js
@@ -10,7 +10,6 @@ const Home = () => {
       {/* Navigation Bar */}
       <nav className="navigation-bar">
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
